1d3e29575c5907c37a4697aeeb75d0b504cf59ff
Igor Lucena <ivln@cesar.org.br>
2023-11-06 16:46:14 -0300

    Added download ddi bash file

--- benchmark/download_ddi.sh
+++ benchmark/download_ddi.sh
@@ -3,7 +3,7 @@ wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&i
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1QbpwcJbDf2TPbWf28kr7tmNPZMli5z2w' -O ddi_train.txt
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1MrXUcs_4RcOHzjtumI8lO3tUUYnAS6rw' -O ddi_val.txt
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1oIIBOtTdNe8sq54txe6hh9wARQ7AwC6n' -O ddi_test.txt
-wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=18d8NON7BKNVwTPyprb9N6P9BwhW9sxVh' -O ddi_train_gpt.txt
+wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1ECw-ub-1kdxD9mV1IIl6V6L6wz-TjHa2' -O ddi_train_gpt.txt
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1QNKFFy3uMDjWwtGGTklqLoS30zXPek_0' -O ddi_test_gpt.txt
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=1lowaa1xW8i96-CjisCPyX3jaug8aZlmC' -O ddi_synt_train.txt
 wget --no-check-certificate -P ddi 'https://docs.google.com/uc?export=download&id=19vCZy7142xL7ODHChOno_RzRvcfeo03H' -O ddi_synt_val.txt
